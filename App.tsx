67947db63ed39d2b18fb9fab85e4eaa4b6efcd13
ralph <ralphlouiegregorio@gmail.com>
2022-11-12 19:56:51 -0600

    Added navigation dependencies

--- App.tsx
+++ App.tsx
@@ -2,20 +2,35 @@ import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NativeBaseProvider, Box } from "native-base";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainNavigation from "./src/AppNavigation/MainNavigation";
+import WorkoutPage from "./src/WorkoutsScreen/WorkoutPage";
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+    return (
+        <Box>
+            <Text>Open up App.tsx to start working on your apps!</Text>
+        </Box>
+    );
+}
 
 export default function App() {
   return (
-      <NativeBaseProvider>
-          <View style={styles.container}>
-              <Box>
-                  <Text>Open up App.tsx to start working on your apps!</Text>
-              </Box>
-              <MainNavigation />
-              <StatusBar style="auto" />
-          </View>
-      </NativeBaseProvider>
-
+      <NavigationContainer>
+          <NativeBaseProvider>
+              <View style={styles.container}>
+                  <Stack.Navigator>
+                      <Stack.Screen name={'Workout'} component={WorkoutPage} />
+                      <Stack.Screen name={'Home'} component={HomeScreen} />
+                  </Stack.Navigator>
+                  <MainNavigation />
+                  <StatusBar style="auto" />
+              </View>
+          </NativeBaseProvider>
+      </NavigationContainer>
 );
 }
 
---
+++ src/WorkoutsScreen/WorkoutPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from 'react-native';
+import { Box } from "native-base";
+
+function WorkoutPage() {
+    return (
+        <View>
+            <Box>
+                <Text>Workout!</Text>
+            </Box>
+        </View>
+    );
+}
+
+export default WorkoutPage;
