b79ba95cfc34228b941e228f01ca25323d9398ad
ralph <ralphlouiegregorio@gmail.com>
2022-11-13 16:30:34 -0600

    Added manual app restart

--- src/SettingsScreen/SettingsScreen.tsx
+++ src/SettingsScreen/SettingsScreen.tsx
@@ -5,9 +5,13 @@ import {
     ScrollView,
     Heading,
     Text,
-    VStack
+    VStack,
+    HStack,
+    Button
 } from "native-base";
 import { DarkModeSwitch } from "./components/DarkModeSwitch";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Updates from 'expo-updates';
 
 function SettingsScreen(): ReactElement<typeof ScrollView> {
     const safeAreaProps = useSafeArea({
@@ -16,13 +20,25 @@ function SettingsScreen(): ReactElement<typeof ScrollView> {
         px: 4
     });
 
+    const clearAsyncStorage = async() => {
+        await AsyncStorage.clear();
+        console.warn('App will now be restarted')
+        await Updates.reloadAsync();
+
+    }
+
     return (
         <ScrollView _dark={{ bg: "coolGray.800" }}>
             <Box {...safeAreaProps}>
                 <VStack space={4} alignItems="left">
                     <Heading size={'2xl'}>Settings</Heading>
-                    <Text>SettingsScreen!</Text>
-                    <DarkModeSwitch />
+                    <HStack space={2}>
+                        <Text>Toggle Dark Mode</Text>
+                        <DarkModeSwitch />
+                    </HStack>
+                    <Button onPress={clearAsyncStorage}>
+                        Clear Async Storage and Restart
+                    </Button>
                 </VStack>
             </Box>
         </ScrollView>
