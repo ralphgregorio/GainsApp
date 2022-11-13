8d5ec177a39a2eec8eee5a07ee5552c5f4489aa6
ralph <ralphlouiegregorio@gmail.com>
2022-11-13 14:21:50 -0600

    Created initial datatypes

--- redux/types.ts
+++ redux/types.ts
@@ -1,5 +1,33 @@
 const APP_COLOR_MODE = "app/colorMode";
 
+enum Gains {
+    EXERCISE = "gains/exercise",
+    WORKOUT = "gains/workout"
+}
+
+type Exercise = {
+    id: string,
+    name: string,
+    category: string,
+    bodyPart: string,
+    description: string,
+    url: string,
+    type: Gains.EXERCISE
+}
+type Workout = {
+    id: string,
+    name: string,
+    sets: number,
+    reps: number,
+    duration: number,
+    timestamp: string,
+    exercise: Exercise,
+    type: Gains.WORKOUT
+}
+
 export {
-    APP_COLOR_MODE
+    APP_COLOR_MODE,
+    Gains,
+    Exercise,
+    Workout
 };
